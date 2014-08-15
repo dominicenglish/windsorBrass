00985f2c9a437c75c87ee70a870245e01046172c
Dominic English <dominic.b.english@gmail.com>
2014-08-15 18:55:42 +1000

    feat(c:about): add about module

---
+++ client/modules/about/about-controllers.js
@@ -0,0 +1,22 @@
+angular.module('windsor.about')
+
+.controller('AboutController', ['$scope', function($scope) {
+    $scope.map = {
+        center: {
+            latitude: -27.426871,
+            longitude: 153.032517
+        },
+        zoom: 15,
+        options: {
+            disableDefaultUI: true
+        }
+    };
+    $scope.marker = {
+        id: 'band-hall',
+        coordinates: {
+            latitude: -27.426871,
+            longitude: 153.032517
+        }
+    }
+}])
+;
