36e14506e56840ce26ef1c8f72afd763931e8703
Dominic English <dominic.b.english@gmail.com>
2014-08-15 18:56:07 +1000

    feat(c:contact): add contact module

---
+++ client/modules/contact/contact-services.js
@@ -0,0 +1,13 @@
+angular.module('windsor.contact')
+
+.factory('MessageTransport', ['$resource', function($resource) {
+    return $resource(
+        '/api/message',
+        {},
+        {
+            email: {
+                method: 'POST'
+            }
+        }
+    );
+}])
