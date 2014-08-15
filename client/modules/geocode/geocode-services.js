b06cdfbd77a5c20b92244ff71d12edabde7e0c42
Dominic English <dominic.b.english@gmail.com>
2014-08-15 19:51:07 +1000

    feat(c:geocode): add geocode module

---
+++ client/modules/geocode/geocode-services.js
@@ -0,0 +1,18 @@
+angular.module('windsor.geocode')
+
+.factory('GeocodeResource', ['$resource', 'GoogleApiKey', function($resource, GoogleApiKey) {
+
+    return $resource(
+        'https://maps.googleapis.com/maps/api/geocode/json',
+        {
+            key: GoogleApiKey,
+            address: '@address'
+        },
+        {
+            geocodeAddress: {
+                method: 'GET',
+                cache: true
+            }
+        }
+    );
+}])
