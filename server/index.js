21c58955310636a1b066a82c84a8e4f909b83aef
Dominic English <dominic.b.english@gmail.com>
2014-08-28 17:19:40 +1000

    feat(s:): add API route for subscribing to MailChimp list

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ var express = require('express'),
     viewHelpers = require('view-helpers'),
     nodemailer = require('nodemailer'),
     bodyParser = require('body-parser'),
+    request = require('request'),
     config = require('./config'),
+    mcapi = require('mailchimp-api'),
     app = express(),
     port = 3003;
 
@@ -24,9 +26,6 @@ app.use(bodyParser.json());
 app.use(viewHelpers('windsor-brass'));
 
 // Routes
-app.get('/test', function(req, res) {
-    res.render('test', {});
-});
 app.post('/api/message', function(req, res) {
     var subjectSuffix = req.body.email || req.body.name || (new Date()).toISOString();
 
@@ -53,8 +52,61 @@ app.post('/api/message', function(req, res) {
         }
     });
 });
+app.post('/api/newsletter/subscribe', function(req, res, next) {
+
+    var ALREADY_SUBSCRIBED = 214;
+    var responseMessage = 'There was an error subscribing that user';
+    var emailToSubscribe = req.body.email;
+    if (!emailToSubscribe) {
+        return next(new Error('No email to subscribe was provided.'));
+    }
+
+    var mailChimpRequest = {
+        apikey: config.mailChimpDetails.key,
+        id: config.mailChimpDetails.listId,
+        batch: [
+            {
+                email: {
+                    email: emailToSubscribe
+                },
+                email_type: 'html',
+                merge_vars: {}
+            }
+        ],
+        double_optin: false,
+        update_existing: false,
+        replace_interests: false
+    };
+
+    request.post(
+        {
+            uri: 'https://us2.api.mailchimp.com/2.0/lists/batch-subscribe.json',
+            json: mailChimpRequest
+        },
+        function(error, response, body) {
+            if (error) {
+                return next(new Error(responseMessage));
+            }
+            if (body && body.errors && body.errors.length ) {
+                if (body.errors[0].code === ALREADY_SUBSCRIBED) {
+                    return next(body.errors[0]);
+                }
+                return next(new Error(responseMessage));
+            }
+            res.send(200, 'Success, user was subscribed to mailing list');
+        }
+    );
+});
 app.all('*', function(req, res) {
     res.render('index', {});
+});
+
+// Error handling
+app.use(function(err, req, res, next) {
+    if (err) {
+        return res.send(500, err);
+    }
+    next(err);
 });
 
 //Assume 404 since no middleware responded
