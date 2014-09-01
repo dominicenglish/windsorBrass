af8d09bba2feb3a343750d8174a406ab98782223
Dominic English <dominic.b.english@gmail.com>
2014-09-01 14:02:21 +1000

    feat(c:contact): add error handling for contact form
    - Set all fields to dirty if submition is invalid
    - Wipe errors on submition attempt
    - Wipe form values on successfull submition
    - Move errors to custom object to prevent them from showing up on load
    - Add error markup for common errors

--- client/modules/contact/contact-controllers.js
+++ client/modules/contact/contact-controllers.js
@@ -1,23 +1,29 @@
 angular.module('windsor.contact')
 
 .controller('ContactController', ['$scope', 'MessageTransport', function($scope, MessageTransport) {
-    $scope.sender = {name: 'Dom', email: 'dominic.b.english@gmail.com', message: 'hello there'};
-    $scope.response = {completed: false, message: ''};
+    $scope.sender = {};
     $scope.sendMessage = function() {
-        if ($scope.sender.message) {
-            $scope.success = false;
-            $scope.error = false;
-            MessageTransport.email($scope.sender).$promise
-                .then(function(response) {
-                    $scope.response.completed = true;
-                    $scope.response.message = "Your message was successfully sent";
-                    $scope.sender = {};
-                })
-                .catch(function(response) {
-                    $scope.response.completed = true;
-                    $scope.response.message = "Something has gone wrong. Your message was not sent";
+        $scope.contactForm.displayErrors = $scope.contactForm.$error;
+        if (!$scope.sender.message) {
+            $scope.contactForm.$setDirty();
+            angular.forEach($scope.contactForm.$error, function(errorType){
+                angular.forEach(errorType, function(field) {
+                    // Set fields to dirty
+                    field.$setViewValue(field.$viewValue);
                 });
+            });
+            return;
         }
+        // Wipe any form errors
+        $scope.contactForm.displayErrors = {};
+        MessageTransport.email($scope.sender).$promise
+            .then(function(response) {
+                $scope.contactForm.displayErrors.serverSuccess = true;
+                $scope.sender = {};
+            })
+            .catch(function(response) {
+                $scope.contactForm.displayErrors.serverError = true;
+            });
     }
 }])
 ;
