3312cf3279308e3e3013ad7ecfbf59934ec32641
Dominic English <dominic.b.english@gmail.com>
2014-10-03 08:52:53 +1000

    feat(c:config): add config module with API constants

---
+++ client/modules/config/config-services.js
@@ -0,0 +1,6 @@
+angular.module('windsor.config')
+
+.constant('GoogleApiKey', 'AIzaSyCA_lBJD_eqsxjQ22fFHkGu8zTW8xTu3Fc')
+.constant('GoogleCalendarId', 'ausbrass.com_7ff0icqnt45drrqqtun4eojl08@group.calendar.google.com')
+
+;
