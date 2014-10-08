d151ce8ddd77f9944d6154749037b4dcd3ed572f
Dominic English <dominic.b.english@gmail.com>
2014-10-08 13:39:43 +1000

    fix(c:test): various fixes for geocode-services
    - Remove accidental parameter in beforeEach statement
    - Refactor maps API url template into its own variable
    - Add test to check for a returned promise
    - Add test to check that returned promise is valid
    - Add test to make sure that promise gets rejected if a bad request is made

--- test/unit/geocode-services-test.js
+++ test/unit/geocode-services-test.js
@@ -13,9 +13,10 @@ describe('geocode-services', function() {
         fakeGeocodeAddress = 'Shalom Performing arts Centre, Shalom College campus, 9 Fitzgerald St, Bundaberg QLD, Australia';
 
         GoogleApiKey = 'Ab_123456789';
+        urlTemplate = /https:\/\/maps\.googleapis\.com\/maps\/api\/geocode\/json\?address=[\w\s,\+]*&key=[\w]*/;
         expectedUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address='+fakeGeocodeAddress.replace(/\s/g, '+')+'&key='+GoogleApiKey;
 
-        beforeEach(function($provide) {
+        beforeEach(function() {
             module(function($provide) {
                 $provide.constant('GoogleApiKey', GoogleApiKey);
             });
@@ -24,7 +25,7 @@ describe('geocode-services', function() {
                 // Set up mock server
                 $httpBackend = _$httpBackend_;
                 GeocodeResource = _GeocodeResource_;
-            })
+            });
         });
 
         afterEach(function() {
@@ -37,8 +38,7 @@ describe('geocode-services', function() {
         });
 
         it('geocodeAddress should make a get request', function() {
-            var regex = /https:\/\/maps\.googleapis\.com\/maps\/api\/geocode\/json\?address=[\w\s,\+]*&key=[\w]*/;
-            $httpBackend.expectGET(regex).respond();
+            $httpBackend.expectGET(urlTemplate).respond();
             var response = GeocodeResource.geocodeAddress({address:fakeGeocodeAddress});
             $httpBackend.flush();
         });
@@ -46,6 +46,34 @@ describe('geocode-services', function() {
         it('geocodeAddress should invoke the request with the right parameters', function() {
             $httpBackend.expectGET(expectedUrl).respond();
             var response = GeocodeResource.geocodeAddress({address:fakeGeocodeAddress});
+            $httpBackend.flush();
+        });
+
+        it('should return an object with a $promise property', function() {
+            $httpBackend.when('GET', urlTemplate).respond();
+            var response = GeocodeResource.geocodeAddress({address:fakeGeocodeAddress});
+            $httpBackend.flush();
+            expect(response.$promise).toBeDefined();
+        });
+
+        it('returned $promise property should be a valid promise', function() {
+            $httpBackend.when('GET', urlTemplate).respond();
+            var response = GeocodeResource.geocodeAddress({address:fakeGeocodeAddress});
+            $httpBackend.flush();
+            expect(angular.isFunction(response.$promise.then)).toBe(true);
+            expect(angular.isFunction(response.$promise.finally)).toBe(true);
+            expect(angular.isFunction(response.$promise.catch)).toBe(true);
+        });
+
+        it('should return a rejected promise if connection fails', function(done) {
+            $httpBackend.expectGET(urlTemplate).respond(401, '');
+            var response = GeocodeResource.geocodeAddress({address:fakeGeocodeAddress});
+
+            response.$promise
+                .catch(function(reason) {
+                    expect(reason).toBeDefined();
+                    setTimeout(done, 0);
+                });
             $httpBackend.flush();
         });
     });
